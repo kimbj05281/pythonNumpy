fb89fdf8620b15dda3cc817a57e1494becbee1e2
Beomjun Kim <kimbj05281@naver.com>
2024-08-13 18:36:17 +0900

    first commit

--- index.py
+++ index.py
@@ -3,3 +3,13 @@ import numpy as np
 
 narray = np.array([[1,3,5,7,9],[2,4,6,8,10]])
 print(narray.shape)
+d52 = narray.reshape(5,2)
+print(d52.shape)
+d10 = narray.reshape(10,)
+print(d10.shape)
+
+zero = np.zeros((2,5))
+print(zero)
+
+one = np.ones((2,5))
+print(one)
