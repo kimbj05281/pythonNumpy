4efbb7221b1f7ceb204f300e72dd6460c4d2fd6d
Beomjun Kim <kimbj05281@naver.com>
2024-08-15 17:04:41 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,13 @@
+five = [5,3,2,4,5]
+
+array = range(9,-1,-1)
+print(list(array))
+
+for i in range(1,6):
+    print(i)
+
+num = 9
+if num >= 10:
+    print("nigga MOVE!")
+elif num < 10:
+    print("hell nah")
